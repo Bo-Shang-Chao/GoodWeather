7ffc797893bbac323cd4d8e9f6ac43751ec7de59
Sean <seanbopo@gmail.com>
2022-05-15 12:39:36 +0800

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by ChaoBo-Shang on 2022/5/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
